eae9784bfb777052ffd05b5aab30b74e60ab5fe1
georgiyu <posysaev20001@mail.ru>
2020-12-13 20:29:15 +0300

    Revert "Сайт"
    This reverts commit 3e4f6c067f10b74c8901abda84b10c1729bfc20c.

--- Сайт/dist/src/js/mars.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	 $('.menu-btn').on('click', function(){
-    $('.menu-block').toggleClass('menu_block_active');
- });
- $('.menu-btn').on('click', function(e){
-  e.preventDefault;
-  $(this).toggleClass('menu-btn_active');
- });
-  });
